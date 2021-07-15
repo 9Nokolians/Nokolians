d9b7b91f155c27b89ba0b4bfce3264312381ec1f
Nokolians <201000322e@eastdelta.edu.bd>
2021-07-15 15:17:24 +0600

    all added

---
+++ index.js
@@ -0,0 +1,76 @@
+var a=document.getElementById("akshoy");
+var b=document.getElementById("iftakher");
+var c=document.getElementById("nowsin");
+var d=document.getElementById("robin");
+var e=document.getElementById("metu");
+var f=document.getElementById("israt");
+var g=document.getElementById("uddin");
+var h=document.getElementById("humaira");
+var i=document.getElementById("himu");
+i.addEventListener("click",function () {
+    showHim();
+
+});
+a.addEventListener("click",function () {
+    
+    showAkshoy();
+});
+h.addEventListener("click",function () {
+    
+    showHum();
+});
+b.addEventListener("click",function () {
+    showIf();
+
+});
+c.addEventListener("click",function () {
+    showNow();
+
+});
+d.addEventListener("click",function () {
+    showRobin();
+
+});
+e.addEventListener("click",function () {
+    showMetu();
+
+});
+f.addEventListener("click",function () {
+    showIsrat();
+   
+});
+g.addEventListener("click",function () {
+    showUddin();
+ 
+});
+
+function showRobin() {
+    window.open("https://www.facebook.com/leoAbdulHamidRobin");
+}
+function showMetu() {
+    window.open("https://www.facebook.com/mk.metu.9");
+}
+function showHum() {
+    window.open("https://www.facebook.com/profile.php?id=100010156092758");
+}
+function showHim() {
+    window.open("https://www.facebook.com/profile.php?id=100007299160786");
+}
+function showIsrat() {
+    window.open("https://www.facebook.com/profile.php?id=100015080555232");
+}
+function showUddin() {
+    window.open("https://www.facebook.com/mohiuddindipu91");
+}
+function showIf() {
+    window.open("https://www.facebook.com/iftakher.hasan.7");
+}
+function showNow() {
+    window.open("https://www.facebook.com/nowshin.proma");
+}
+
+function showAkshoy() {
+    window.open("https://www.facebook.com/profile.php?id=100009467118663");
+    
+}
+
---
+++ nokol.js
@@ -0,0 +1,10 @@
+var x=document.getElementsById("nokol1");
+x.addEventListener("click",function()
+{
+main();
+});
+function main()
+{
+    alert("clicked");
+    location.replace("htteps://www.facebook.com");
+}
